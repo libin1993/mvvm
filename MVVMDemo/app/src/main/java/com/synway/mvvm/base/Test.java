918f97372d54dc6b5eeb3390d1453f9d10f9afd1
libin <1993911441@qq.com>
2021-08-11 09:57:20 +0800

    33333

--- MVVMDemo/app/src/main/java/com/synway/mvvm/base/Test.java
+++ MVVMDemo/app/src/main/java/com/synway/mvvm/base/Test.java
@@ -23,7 +23,7 @@ import rx.Subscriber;
  * Describe：
  */
 class Test {
-    private static final String TAG = "libin2222";
+    private static final String TAG = "libin33333";
 
     public static void main(String[] args) {
 //        System.out.println(romanToInt("II"));
